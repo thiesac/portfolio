2368d4b648839ae4ad13b8962b13b9df3dad5a2e
Thiesa <thiesa.c@gmail.com>
2023-11-14 16:19:21 -0400

    style(projects)

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -10,15 +10,15 @@ function Projects() {
     return (
         <div className="projects-container">
             <Row className="g-4 mx-md-auto">
-                <h3 style={ { color: '#1D1842' } } className="mb-4 text-center">
+                <h2 style={ { color: '#1D1842' } } className="projects-header my-5 text-center">
                     Check out some of my projects
-                </h3>
+                </h2>
                 { projects.map((project, index) => (
                     <Col
                         key={ index }
                         xs={ 12 } // full width on extra-small screens
                         md={ 6 }  // half width on medium screens
-                        lg={ 4 }  // one-third width on large screens
+                        lg={ 2 }  // one-third width on large screens
                     >
                         <ProjectCard project={ project } />
                     </Col>
--- src/pages/Projects/ProjectsList.jsx
+++ src/pages/Projects/ProjectsList.jsx
@@ -5,7 +5,11 @@ import sound from '../../images/sound.png';
 import rpg from '../../images/rpg.png';
 import soccer from '../../images/soccer.png';
 import recipes from '../../images/recipes.png';
-import search from '../../images/search.png'
+import search from '../../images/search.png';
+import github from '../../images/github.png';
+import manager from '../../images/manager.png';
+import orders from '../../images/orders.png';
+import algorithms from '../../images/algorithms.png';
 
 const projects = [
     {
@@ -56,46 +60,31 @@ const projects = [
         link: 'https://github.com/thiesac/top-trumps-card-game',
         image: card,
     },
-    // {
-    //     name: 'SQL All For One',
-    //     technologies: 'Node.js, MySQL',
-    //     link: 'https://github.com/thiesac/sql-all-for-one',
-    // },
-    // {
-    //     name: 'Blogs API',
-    //     technologies: 'Node.js, Express, MongoDB',
-    //     link: 'https://github.com/thiesac/blogs-api',
-    // },
-    // {
-    //     name: 'MySQL One For All',
-    //     technologies: 'Node.js, MySQL',
-    //     link: 'https://github.com/thiesac/mysql-one-for-all',
-    // },
-    // {
-    //     name: 'Docker Todo List',
-    //     technologies: 'React, Node.js, Docker',
-    //     link: 'https://github.com/thiesac/docker-todo-list',
-    // },
-    // {
-    //     name: 'Medieval Craft API',
-    //     technologies: 'Node.js, Express',
-    //     link: 'https://github.com/thiesac/medieval-craft-api',
-    // },
-    // {
-    //     name: 'Store Manager',
-    //     technologies: 'React, Redux, Node.js, Express',
-    //     link: 'https://github.com/thiesac/store-manager',
-    // },
-    // {
-    //     name: 'Talker Manager',
-    //     technologies: 'React, Redux, Node.js, Express',
-    //     link: 'https://github.com/thiesac/talker-manager',
-    // },
-    // {
-    //     name: 'Project Algorithms',
-    //     technologies: 'JavaScript',
-    //     link: 'https://github.com/thiesac/project-algorithms',
-    // },
+    {
+        name: 'Talker Manager',
+        technologies: 'Node.js, Docker, API REST, MYSQL',
+        link: 'https://github.com/thiesac/talker-manager',
+        image: manager,
+    },
+    {
+        name: 'Project Algorithms',
+        technologies: 'Python, OOP, Algorithms',
+        link: 'https://github.com/thiesac/project-algorithms',
+        image: algorithms,
+    },
+    {
+        name: 'Restaurant Orders',
+        technologies: 'Python, OOP, Algorithms',
+        link: 'https://github.com/thiesac/restaurant-orders',
+        image: orders,
+    },
+    {
+        name: 'All projects',
+        technologies: 'My Github profile',
+        link: 'https://github.com/thiesac',
+        image: github,
+    },
+    
 ];
 
 export default projects;
