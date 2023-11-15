ce244076f6ae97febdb93e0ee82f1105abb8aefe
Thiesa <thiesa.c@gmail.com>
2023-11-15 14:40:37 -0400

    chore(AboutMe): update

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -8,12 +8,14 @@ function AboutMe() {
             <h3>About Me</h3>
             <h4>Hi, I'm Thiesa, a passionate web developer from Brazil. 👩‍💻</h4>
             <p>
-                Enrolled in <a href="https://programaria.org/" target="_blank" rel="noopener noreferrer">PrograMaria</a>,
-                a tech course empowering women. Instantly fell in love with coding.
+                My journey into web development started when I enrolled in <a href="https://programaria.org/" target="_blank" rel="noopener noreferrer">PrograMaria</a>,
+                a tech course empowering women, and I instantly fell in love with coding and the community.
+            </p>
+            <p>
                 Excited by problem-solving, my skills include Python, Node.js, React.js, and MYSQL. 💡
             </p>
             <p>
-                Graduating from Trybe in March 2024, I've tackled diverse projects, valuing continuous learning,
+                Graduating from <a href="https://www.betrybe.com/">Trybe</a> in March 2024, I've tackled diverse projects, valuing continuous learning,
                 quality work, and meaningful connections. 🚀
             </p>
             <p>
